a548ad27926194730e5a54065d136a49c9e6efa3
dhingratul <atul.dhingra@rutgers.edu>
2017-06-18 13:04:47 -0400

    Cluster Function

--- cluster.m
+++ cluster.m
@@ -1,6 +1,7 @@
 %% Clustering
 % Input: weight vector, output label, counter
 % Output: Output label, counter
+% Note: Run from main.m
 function[outlabel,ctr]=cluster(vector,outlabel,ctr)
 A=find(vector~=0)
 for i=1:size(A,2)
